f55218fa33fc4ccdef2f742c1990b6ae8b0afe00
hadi-a-azeez <hadikoduvally@gmail.com>
2021-03-24 00:26:55 +0530

    initial setup

--- pages/index.js
+++ pages/index.js
@@ -1,209 +1,104 @@
-import Head from 'next/head'
+import "tailwindcss/tailwind.css";
+import Card from "../components/card";
 
-export default function Home() {
+const Home = () => {
   return (
-    <div className="container">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <div className="md:container md:mx-auto">
+      {/* header starts here */}
+      <div class="flex flex-row justify-between p-4 mt-3">
+        <h1 className="text-lg font-bold text-gray-600">Community</h1>
+        <h1 className="text-sm font-normal text-gray-600">Join Now</h1>
+      </div>
+      {/* header ends here */}
 
-      <main>
-        <h1 className="title">
-          Learn <a href="https://nextjs.org">Next.js!</a>
+      <div class="flex flex-col items-center w-full mt-16">
+        <h1 className="font-sans text-4xl font-bold text-center text-gray-800 leading-tight w-11/12">
+          A Learning Community For Mamo College
         </h1>
+        <h1 className="font-sans text-xl font-normal text-center text-gray-500 leading-tight mt-3 w-11/12">
+          Help you accelerate your learning, and bring you closer to like-minded
+          individuals, who could all be a valuable asset in your journey
+        </h1>
+        <button className="mt-3 pl-7 pr-7 p-2 bg-yellow-400 rounded font-semibold">
+          JOIN NOW
+        </button>
+        <img src="/humans_all.png" className="w-full h-auto mt-7" alt="im" />
+      </div>
 
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+      {/* cards starts here */}
+      <div
+        class="flex flex-row items-center w-full mt-3 mb-6 overflow-scroll p-8"
+        style={{ scrollSnapType: "x mandatory" }}
+      >
+        <Card
+          heading="Peer-to-peer mentorship"
+          subHeading="Improve communication and interpersonal skills"
+          image="/peertopeer.png"
+        />
+        <Card
+          heading="Ideas into real world projects"
+          subHeading="Hands on experience in real world projects"
+          image="real_world.png"
+        />
+        <Card
+          heading="Peer-to-peer mentorship"
+          subHeading="Improve communication and interpersonal skills"
+          image="/peertopeer.png"
+        />
+        <Card
+          heading="Peer-to-peer mentorship"
+          subHeading="Improve communication and interpersonal skills"
+          image="/peertopeer.png"
+        />
+      </div>
+      {/* cards ends here */}
 
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+      {/* waves starts here */}
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <path
+          fill="#f1edff"
+          fill-opacity="1"
+          d="M0,64L60,96C120,128,240,192,360,208C480,224,600,192,720,165.3C840,139,960,117,1080,128C1200,139,1320,181,1380,202.7L1440,224L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"
+        ></path>
+      </svg>
+      <div
+        className="w-full flex flex-col items-center "
+        style={{
+          background:
+            "linear-gradient(360deg, rgba(255,255,255,1) 9%, rgba(241,237,255,1) 42%, rgba(241,237,255,1) 30%)",
+          height: "auto",
+        }}
+      >
+        <img src="/invite.png" className="w-full h-auto" alt="im" />
+        <h1 className="font-sans text-2xl font-bold text-center text-gray-700 leading-tight w-11/12">
+          Invite your friends to join the community
+        </h1>
+        <button className="mt-4 mb-3 pl-7 pr-7 p-2 bg-yellow-400 rounded font-semibold">
+          INVITE NOW
+        </button>
+      </div>
+      {/* waves ends here */}
 
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
+      {/* rounded rectangle starts here */}
+      <div
+        className="flex flex-col items-center p-2 w-full mt-6 mb-6"
+        style={{ borderRadius: "50px", background: "#FFF9E8" }}
+      >
+        <img src="/volunteer.png" className="w-full h-auto" alt="im" />
+        <h1 className="font-sans text-2xl font-bold text-center text-gray-700 leading-tight w-11/12">
+          Be a community volunteer
+        </h1>
+        <h1 className="font-sans text-sm font-normal text-center text-gray-500 leading-tight mt-1 w-11/12">
+          There is so much that you can do to this community, Be a volunteer and
+          let's make a change together
+        </h1>
+        <button className="mt-4 mb-3 pl-7 pr-7 p-2 bg-yellow-400 rounded font-semibold">
+          APPLY NOW
+        </button>
+      </div>
+      {/* rounded rectangle ends here */}
     </div>
-  )
-}
+  );
+};
+
+export default Home;
