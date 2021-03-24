872699932b6dac9644e9e15bd7e240d2b7821457
hadi-a-azeez <hadikoduvally@gmail.com>
2021-03-25 01:59:27 +0530

    firebase OTP implementation

--- pages/join/index.js
+++ pages/join/index.js
@@ -2,6 +2,7 @@ import InputField from "../../components/InputField";
 import "tailwindcss/tailwind.css";
 import { connect } from "react-redux";
 import { setInfo } from "../../redux/actions/main";
+import firebase from "../../firebase";
 
 const Join = (props) => {
   const { userInfo, setInfo } = props;
@@ -10,8 +11,54 @@ const Join = (props) => {
     setInfo(value);
   };
 
-  const handleClick = () => {
-    console.log(userInfo.mobileNumber);
+  const setUpRecaptcha = () => {
+    window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier(
+      "recaptcha-container",
+      {
+        size: "invisible",
+        callback: (response) => {
+          // reCAPTCHA solved, allow signInWithPhoneNumber.
+          handleClick();
+        },
+      }
+    );
+  };
+
+  const handleClick = (event) => {
+    event.preventDefault();
+    setUpRecaptcha();
+    const phoneNumber = "+917012179326";
+    const appVerifier = window.recaptchaVerifier;
+    firebase
+      .auth()
+      .signInWithPhoneNumber(phoneNumber, appVerifier)
+      .then((confirmationResult) => {
+        // SMS sent. Prompt user to type the code from the message, then sign the
+        // user in with confirmationResult.confirm(code).
+        console.log("otp sended");
+        window.confirmationResult = confirmationResult;
+        const code = window.prompt("Enter OTP");
+        confirmationResult
+          .confirm(code)
+          .then((result) => {
+            // User signed in successfully.
+            const user = result.user;
+            console.log("user signed in");
+            // ...
+          })
+          .catch((error) => {
+            // User couldn't sign in (bad verification code?)
+            // ...
+            console.log("user did not signed in");
+          });
+
+        // ...
+      })
+      .catch((error) => {
+        // Error; SMS not sent
+        // ...
+        console.log("otp did not send");
+      });
   };
 
   return (
@@ -26,6 +73,7 @@ const Join = (props) => {
         value={userInfo.mobileNumber}
         onChange={onChangeHandler}
       />
+      <div id="recaptcha-container" />
       <button
         className="mt-3 pl-7 pr-7 p-2 bg-blue-600 text-white rounded-lg font-semibold w-4/5"
         onClick={handleClick}
