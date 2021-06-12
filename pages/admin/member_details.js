2f7be4c31f2f85c728492b849c0b74b1df47d3b8
hadi-a-azeez <hadikoduvally@gmail.com>
2021-06-12 18:27:34 +0530

    ui adding

--- pages/admin/member_details.js
+++ pages/admin/member_details.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { getMemberDetails } from "../../utilities/api";
+import styles from "./member_details.module.css";
 
 const MemberDetails = () => {
   const [member, setMember] = useState([]);
@@ -13,11 +14,22 @@ const MemberDetails = () => {
     getDetails();
   }, []);
   return (
-    <>
-      <h1>{member[0].name}</h1>
-      <h1>{member[0].phone_number}</h1>
-    </>
+    <div className={styles.container}>
+      {/* Header starts here */}
+      <div className={styles.header}>
+        <img src="/back_icon.svg" className={styles.back_icon} />
+        <h1 className={styles.heading}>Member</h1>
+      </div>
+      {/* Header ends here */}
+      <div className={styles.circle} />
+    </div>
   );
 };
 
 export default MemberDetails;
+/* {member.length && (
+        <>
+          <h1>{member[0].name}</h1>
+          <h1>{member[0].phone_number}</h1>
+        </>
+      )} */
