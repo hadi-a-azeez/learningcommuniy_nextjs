7af75e9e84cdd62c39821eb239b8b8872ae178ef
hadi-a-azeez <hadikoduvally@gmail.com>
2021-07-02 18:11:01 +0530

    login function implemented

--- pages/admin/login.js
+++ pages/admin/login.js
@@ -1,21 +1,49 @@
 import InputField from "../../components/InputField";
 import Button from "../../components/button";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import supabase from "../../supabase";
+import { useRouter } from "next/router";
 
 const Login = () => {
   const [userDetails, setUserDetails] = useState({
     email: "",
     password: "",
   });
+  const [isError, setIsError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
   const [isLoading, setIsLoading] = useState(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    //redirect to dashboard if already logined
+    if (localStorage.getItem("token") && localStorage.getItem("loginExpiry")) {
+      if (localStorage.getItem("loginExpiry") > Date.now())
+        //console.log(localStorage.getItem("token"));
+        router.push("/admin/home");
+    }
+  }, []);
 
   const handleSignUpClick = async () => {
     setIsLoading(true);
     const { user, session, error } = await supabase.auth.signIn(userDetails);
-    if (user) console.log(user);
-    if (error) console.log(error);
-    if (session) console.log(session);
+    //if (user) console.log(user);
+
+    //handling login errors
+    if (error) {
+      console.log(error);
+      setIsError(true);
+      setErrorMessage(error.message);
+    }
+    if (session) {
+      //console.log(session);
+
+      //if login details are correct remove existing user
+      localStorage.removeItem("token");
+      //adding new user
+      localStorage.setItem("token", session.access_token);
+      localStorage.setItem("loginExpiry", session.expires_at);
+      router.push("/admin/home");
+    }
     setIsLoading(false);
   };
   return (
@@ -24,6 +52,11 @@ const Login = () => {
       <h1 className="mt-3 text-xl font-bold text-gray-600 font-sans">
         Login to your account
       </h1>
+      {isError && (
+        <h1 className="mt-4 text-xl font-normal text-red-500 font-sans">
+          {errorMessage}
+        </h1>
+      )}
       <InputField
         label="email"
         helperText="eg: Aron Paul"
